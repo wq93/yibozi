2e88dad2a7e8f24d9d8d1e7e57dea3a0520b19f7
wangqi <wangqi@chicv.com>
2019-04-26 18:37:12 +0800

    项目添加antd UI库

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import Link from 'next/link'
-
-export default () => (
-  <ul>
-    <li>
-      <Link href='/b' as='/a'>
-        <a>a</a>
+import React, { Fragment } from 'react';
+import { Button } from 'antd';
+import Link from 'next/link';
+import Head from 'next/head';
+const Home = () => (
+  <Fragment>
+    <Head>
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta charSet='utf-8' />
+      <title>Next-Antd-Scafflod</title>
+      <link rel='stylesheet' href='/_next/static/style.css' />
+    </Head>
+    <Fragment>
+      <h1>我是Next的首页</h1>
+      <Link href='/userList'>
+        <Button type='primary'>用户列表页</Button>
       </Link>
-    </li>
-    <li>
-      <Link href='/a' as='/b'>
-        <a>b</a>
-      </Link>
-    </li>
-  </ul>
-)
+    </Fragment>
+  </Fragment>
+);
+export default Home;
