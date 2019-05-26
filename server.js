e5154e1d5c31f75fc0b51506e08e15125df795da
wangqi <wangqi@chicv.com>
2019-05-26 13:49:29 +0800

    add eslint sucess

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const Koa = require('koa')
-const next = require('next')
-const Router = require('koa-router')
-const bodyParser = require('koa-bodyparser')
-const serve = require('koa-static');
+const Koa = require('koa');
+const next = require('next');
+const Router = require('koa-router');
+// const bodyParser = require('koa-bodyparser');
+// const serve = require('koa-static');
+const port = parseInt(process.env.PORT, 10) || 8000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({dev});
+const handle = app.getRequestHandler();
 
-// const db = require('./server/utils/db');
-const port = parseInt(process.env.PORT, 10) || 8000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({dev})
-const handle = app.getRequestHandler()
+require('./server/utils/db');
 
 app.prepare().then(() => {
-  const server = new Koa()
-  const router = new Router()
+  const server = new Koa();
+  const router = new Router();
 
   router.get('*', async ctx => {
-    await handle(ctx.req, ctx.res)
-    ctx.respond = false
-  })
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
 
   server
     .use(async (ctx, next) => {
-      ctx.res.statusCode = 200
-      await next()
+      ctx.res.statusCode = 200;
+      await next();
     })
     .use(router.routes())
     .listen(port, () => {
-      console.log(`> Ready on PORT:${port}`)
-    })
-})
+      console.log(`> Ready on PORT:${port}`);
+    });
+});
