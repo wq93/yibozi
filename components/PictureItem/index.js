9c5b4686b147684c5c64b9489342a42f8356c124
wangqi <wangqi@chicv.com>
2019-08-23 15:49:44 +0800

    pictureItem add hover

--- components/PictureItem/index.js
+++ components/PictureItem/index.js
@@ -1,5 +1,3 @@
-import { useState, useEffect } from 'react';
-
 import Styled from './index.style';
 
 const ArticleItem = ({ path, title, uuid, handleDeletePicture }) => {
@@ -7,6 +5,8 @@ const ArticleItem = ({ path, title, uuid, handleDeletePicture }) => {
   return (
     <Styled>
       <div className='picture-wrapper' onClick={ () => handleDeletePicture(uuid)}>
+        <div className='mark mark-left'/>
+        <div className='mark mark-right'/>
         <img alt={ title } src={ path } title={ title }/>
       </div>
     </Styled>
--- components/PictureItem/index.style.js
+++ components/PictureItem/index.style.js
@@ -4,19 +4,48 @@ export default styled.li`
    position: relative;
    width: 20%;
    padding: 8px;
-   transform: ${ () => `rotate(${ Math.random() * (-20) + Math.random() * 20 }deg) translateY(${ Math.random() * (50) }px)` };
+   transform: ${ () => `rotate(${ 0 }deg) translateY(${ 0 }px)` };
    div.picture-wrapper {
+     position: relative;
      padding: 4px;
      border: 2px solid #B4A078;
      overflow: hidden;
+     cursor: pointer;
+     .mark {
+       position: absolute;
+       top: 0;
+       width: 100%;
+       height: 100%;
+       background: rgba(255,255,255,0.6);
+       z-index: 2;
+       transition: all 0.8s;
+     }
+     .mark-left {
+       left: -100%;
+     }
+     .mark-right {
+       right: -100%;
+       
+     }
      img {
        display: inline-block;
        width: 100%;
        padding: 2px;
        border: 1px dashed #B4A078;
-       cursor: pointer;
      }
-     
+     &:hover {
+        .mark-left {
+          left: -23%;
+        }
+        .mark-right {
+          animation: markRight .5s;
+          right: -80%;
+        }
+     }
+      @keyframes markRight {
+        from { right: -100%; }
+        50%  { right: calc(-50%); } /* ignored */
+        to   { right: -80%; }
+     }
    }
-   
 `;
